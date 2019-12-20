5d7960b42251678032d884e106c02cf3d6935f68
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2019-12-20 14:38:17 +0000

    Update dbus interfaces to match new api

--- wpa-client.h
+++ wpa-client.h
@@ -2,9 +2,9 @@
 #ifndef WPA_CLIENT_H
 #define WPA_CLIENT_H
 
-#define WPAS_DBUS_SERVICE   "fi.epitest.hostap.WPASupplicant"
-#define WPAS_DBUS_PATH      "/fi/epitest/hostap/WPASupplicant"
-#define WPAS_DBUS_INTERFACE "fi.epitest.hostap.WPASupplicant"
+#define WPAS_DBUS_SERVICE   "fi.w1.wpa_supplicant1"
+#define WPAS_DBUS_PATH      "/fi/w1/wpa_supplicant1"
+#define WPAS_DBUS_INTERFACE "fi.w1.wpa_supplicant1"
 
 #define WPAS_DBUS_PATH_INTERFACES   WPAS_DBUS_PATH "/Interfaces"
 #define WPAS_DBUS_IFACE_INTERFACE   WPAS_DBUS_INTERFACE ".Interface"
